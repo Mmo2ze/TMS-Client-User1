a5a82a749c3a08132579353d3b88e59f15402781
Ahmed Mohsen <99752390+Mmo2ze@users.noreply.github.com>
2024-02-24 22:51:28 +0200

    fix:try to fix production

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,21 +5,20 @@ import { routes } from './app.routes'
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http'
 import { provideState, provideStore } from '@ngrx/store'
 import { provideStoreDevtools } from '@ngrx/store-devtools'
-import { authFeatureKey, authReducer } from './store/reducers';
-import { provideNzIcons } from './icons-provider'
+import { authFeatureKey, authReducer } from './store/reducers'
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes),
-        provideHttpClient(withFetch()),
-        provideStore(),
-        provideState(authFeatureKey, authReducer),
-        provideStoreDevtools({
-            maxAge: 25, // Retains last 25 states
-            logOnly: !isDevMode(), // Restrict extension to log-only mode
-            autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-            trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-            traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-        }), provideNzIcons(),
-    ],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideStore(),
+    provideState(authFeatureKey, authReducer),
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
+  ],
 }
